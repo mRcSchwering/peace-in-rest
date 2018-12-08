2baa741f28c42b54d6b8d0235ae6cea571a49e87
Marc Schwering <schweringmarc01@gmail.com>
2018-12-08 21:45:32 +0100

    REST api w/ swagger and authentication

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # peace-in-rest
+
+```
+curl -i -u u1:asd -d 'shoutout=123' http://localhost:5000/mirror
+```
--- app.py
+++ app.py
@@ -1,96 +1,79 @@
-from flask import Flask, session, redirect, url_for, escape, request, jsonify
+from flask import Flask
+from flask_restplus import Resource, Api, reqparse
+from flask_httpauth import HTTPBasicAuth
 import json
+
+# TODO: scaling with swagger
+# https://flask-restplus.readthedocs.io/en/latest/scaling.html
+# and
+# http://michal.karzynski.pl/blog/2016/06/19/building-beautiful-restful-apis-using-flask-swagger-ui-flask-restplus/
+
+# TODO: ssl
+# https://blog.miguelgrinberg.com/post/running-your-flask-application-over-https
+
+# TODO: token-based
+# https://blog.miguelgrinberg.com/post/restful-authentication-with-flask
 
 config_file = 'config.json'
 
 
 # Setup
+app = Flask(__name__)
+api = Api(app, version='1.0', title='PIR')
+log = app.logger
+parser = reqparse.RequestParser()
+auth = HTTPBasicAuth()
+ns = api.namespace('api', description='description for mirror')
+
 with open(config_file) as inf:
     config = json.load(inf)
 
-app = Flask(__name__)
-log = app.logger
 app.secret_key = config['flaskSecretKey']
 data = config['data']
 
 
-# Pages
-def login_page(error):
-    return '''
-        <form method="post">
-            <p><input type=text name=username>
-            <p><input type=text name=password>
-            <p><input type=submit value=Login>
-        </form>
-        <div style="color: red">%s</div>
-    ''' % error
-
-
-def index_page(user):
-    return 'Logged in as %s' % escape(user)
-
-
-# Misc
-def login_is_valid(user, password):
-    accounts = [a for a in config['accounts'] if a['user'] == user]
-    if len(accounts) is not 1:
-        return False
-    if accounts[0]['password'] != password:
-        return False
-    return True
-
-
-# Actions
-def log_user_in():
-    session['username'] = request.form['username']
-    return redirect(url_for('index'))
-
-
-def log_user_out():
-    session.pop('username', '')
-    return redirect(url_for('login'))
+@auth.verify_password
+def verify_password(username, password):
+    for account in config['accounts']:
+        if account['user'] == username:
+            if account['password'] == password:
+                return True
+    return False
 
 
 # Endpoints
-@app.route('/')
-def index():
-    if 'username' not in session:
-        return redirect(url_for('login'))
-    return index_page(session['username'])
+@ns.route('/data')
+class Data(Resource):
+    '''Look at all records'''
+
+    @auth.login_required
+    def get(self):
+        '''Get all records'''
+        return data
 
 
-@app.route('/data')
-def get_all_data():
-    if 'username' not in session:
-        return redirect(url_for('login'))
-    return jsonify(data)
+@ns.route('/data/<string:name>')
+class Record(Resource):
+    '''Look at specific records'''
+
+    @auth.login_required
+    def get(self, name):
+        '''Get record by name'''
+        return [d for d in data if d['name'] == name]
 
 
-@app.route('/data/<string:name>')
-def get_data_for_record(name):
-    if 'username' not in session:
-        return redirect(url_for('login'))
-    return jsonify([d for d in data if d['name'] == name])
+@ns.route('/mirror')
+class Mirror(Resource):
+    '''Nonsense endpoint'''
+
+    @auth.login_required
+    def post(self):
+        '''Return whatever you posted as shoutout'''
+        parser.add_argument('shoutout', type=str, help='Some message')
+        args = parser.parse_args(strict=True)
+        return args
 
 
-@app.route('/logout')
-def logout():
-    if 'username' not in session:
-        return redirect(url_for('login'))
-    log.debug('Logout - user: %s - successful' % session['username'])
-    return log_user_out()
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = ''
-    if request.method == 'POST':
-        user = request.form['username']
-        password = request.form['password']
-        if login_is_valid(user, password):
-            log.debug('Login - user: %s - successful' % user)
-            return log_user_in()
-        else:
-            log.debug('Login - user: %s - failed' % user)
-            error = 'Invalid username/password'
-    return login_page(error)
+#api.add_resource(Data, '/data', '/', endpoint='data')
+#api.add_resource(Record, '/data/<string:name>', '/', endpoint='record')
+#api.add_resource(Mirror, '/mirror', '/', endpoint='mirror')
