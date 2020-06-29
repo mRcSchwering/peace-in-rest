b7c9b435883958074d2c0200c299a2f22313e8c9
Marc Schwering <schweringmarc01@gmail.com>
2020-06-29 22:39:52 +0200

    added some graphql queries

--- app/graphql/schema.py
+++ app/graphql/schema.py
@@ -1,16 +1,37 @@
-import graphene
+from graphene import String, Field, Schema, ObjectType
 from fastapi import APIRouter
 from starlette.graphql import GraphQLApp
 
 router = APIRouter()
 
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(name=graphene.String(default_value="stranger"))
+class Person(ObjectType):
+    """
+    Person can be greeted
+    """
+    first_name = String(description="a first name")
+    last_name = String(default_value="Griffin")
+    full_name = String()
+    greet = String(say=String(default_value="hi"),
+                   description="greet a person")
 
-    def resolve_hello(self, info, name):
-        print(info)
-        return "Hello " + name
+    @staticmethod
+    def resolve_full_name(parent, info):
+        return f"{parent.first_name} {parent.last_name}"
+
+    @staticmethod
+    def resolve_greet(parent, info, say):
+        return f"{say} {parent.first_name} {parent.last_name}"
 
 
-router.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
+class Query(ObjectType):
+    """Root Query"""
+    me = Field(Person, description="Who am I")
+
+    @staticmethod
+    def resolve_me(parent, info):
+        return Person(first_name="Peter")
+
+
+router.add_route(
+    "/", GraphQLApp(schema=Schema(query=Query)))
